dbc5995e6d003082fdf97cbfa7c8ba51f3951a1a
Harsh Deep <hrshdeep51@gmail.com>
2017-01-23 18:24:05 +0530

    Add ActionCable groundwork for chatroom
    -[Channels - Room] Create new channel called room_channel
    1) room_channel.rb - Add stream for code to indicate the particular channel to listen to.
    
    2) room.coffee - Add code to display the message content as an alert
    
    -[Controllers - Messages - Create] Replace earlier update code with code to send data along the 'room_channel' instead. Removed the else branch since it's no longer needed.
    
    -[Routes] Mount ActionCable server at '/cable'

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,12 @@ class MessagesController < ApplicationController
   def create
     message = current_user.messages.build(message_params)
     if message.save
-      redirect_to messages_url
-    else
-      render 'index'
+      ActionCable.server.broadcast 'room_channel',
+                                   content: message.content,
+                                   username: message.user.username
+
+      # Not too sure what this line does, but let's see
+      head :ok
     end
   end
 
